8bcdfefc5b9cb259c353729209c6d5108a04d45b
jilianpetta <117893608+jilianpetta@users.noreply.github.com>
2022-12-15 16:24:11 +0700

    Add files via upload

---
+++ UG13_C_71220868.py
@@ -0,0 +1,38 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+Traceback (most recent call last):
+  File "D:/tes J.py", line 2, in <module>
+    pen.goto
+NameError: name 'pen' is not defined. Did you mean: 'len'?
+
+============================= RESTART: D:/tes J.py =============================
+Traceback (most recent call last):
+  File "D:/tes J.py", line 16, in <module>
+    t = turtle.turtle()
+NameError: name 'turtle' is not defined. Did you mean: 'tuple'?
+
+============================= RESTART: D:/tes J.py =============================
+Traceback (most recent call last):
+  File "D:/tes J.py", line 16, in <module>
+    t = turtle.turtle()
+NameError: name 'turtle' is not defined. Did you mean: 'tuple'?
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
+
+============================= RESTART: D:/tes J.py =============================
